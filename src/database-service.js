aba52ca0b91e8146edb1370bdd7df88687486149
Klemens <wisserklemens@gmail.com>
2022-10-12 19:53:19 +0200

    Removed database service

--- src/database-service.js
+++ src/database-service.js
@@ -1,5 +1,4 @@
 const {downloadFile, uploadFile} = require('./upload-to-cloud-storage');
-const pool_db = require("./pooldb.js")
 
 async function handlePictures(body) {
     const id = body['_id'];
@@ -23,26 +22,7 @@ async function handlePictures(body) {
             console.log(`Error: ${id} ${exception}`)
         }
     }
-    await changePicURL(uploadTasks, amount_of_pictures, id, fileNames)
 }
-
-async function changePicURL(uploadTasks, amount_of_pictures, id, fileNames) {
-    try {
-        await Promise.all(uploadTasks).then(async values => {
-            for (let uploadResult in values) {
-                const current_url = "https://storage.googleapis.com/yourprojektname/" + fileNames[uploadResult]
-                const updateUrlQuery = `INSERT INTO links (link) VALUES ('${current_url}')`
-                console.log(updateUrlQuery)
-                await pool_db.query(updateUrlQuery)
-            }
-        })
-    } catch (e) {
-        console.log("Error: ", id, " ", e)
-    }
-    global.picturesToDownload -= amount_of_pictures
-    console.info("Successful added ", amount_of_pictures, " Pictures for ", id)
-}
-
 
 module.exports = {
     handlePictures
